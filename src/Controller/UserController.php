62e82dd66549da4906be45ddc641df14f2835c68
dimitri78700 <dimitriraievsky@icloud.com>
2025-03-12 14:57:37 +0100

    MAJ UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,16 +2,71 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class UserController extends AbstractController{
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+#[Route('/api/users', name: 'user_')]
+class UserController extends AbstractController
+{
+    /**
+     * Liste tous les utilisateurs.
+     * @Route("/", name="list", methods={"GET"})
+     */
+    #[Route('/', name: 'list', methods: ['GET'])]
+    public function list(UserRepository $userRepository): JsonResponse
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        return $this->json($userRepository->findAll());
+    }
+
+    /**
+     * Récupère un utilisateur spécifique par son ID.
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(User $user): JsonResponse
+    {
+        return $this->json($user);
+    }
+
+    /**
+     * Crée un nouvel utilisateur.
+     * @Route("/create", name="create", methods={"POST"})
+     */
+    #[Route('/create', name: 'create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        // Récupération et décodage des données envoyées en JSON
+        $data = json_decode($request->getContent(), true);
+
+        // Création de l'utilisateur
+        $user = new User();
+        $user->setName($data['name'])
+             ->setEmail($data['email'])
+             ->setPassword(password_hash($data['password'], PASSWORD_BCRYPT)) // Hash du mot de passe
+             ->setRole($data['role']);
+
+        // Persistance et enregistrement en base de données
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Utilisateur créé avec succès'], 201);
+    }
+
+    /**
+     * Supprime un utilisateur par son ID.
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     */
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(User $user, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Utilisateur supprimé']);
     }
 }
