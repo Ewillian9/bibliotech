4700e909e1d54eb67710211f6d36dab26fd4ac93
moi <janonguittard@gmail.com>
2025-03-12 16:16:06 +0100

    make homepage and book with clickable card and book details

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BookController extends AbstractController{
-    #[Route('/book', name: 'app_book')]
-    public function index(): Response
+    #[Route('/book/{id}', name: 'book_show')]
+    public function index(Book $book): Response
     {
         return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController',
+            'book' => $book,
         ]);
     }
 }
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(BookRepository $bookRepository): Response
     {
+        $books = $bookRepository->findAll();
+
         return $this->render('page/index.html.twig', [
             'titrePage' => 'Bienvenue sur Bibliotech !',
+            'books' => $books, // Pass books to the template
         ]);
     }
 
