2586121067ccce1b2fc4a52e80cbca2c50fda4a8
moi <janonguittard@gmail.com>
2025-03-12 16:35:39 +0100

    add loan feature and add more details to home cards and details page

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BookController extends AbstractController{
@@ -15,5 +17,20 @@ final class BookController extends AbstractController{
         return $this->render('book/index.html.twig', [
             'book' => $book,
         ]);
+    }
+
+    #[Route('/book/{id}/loan', name: 'book_loan')]
+    public function loan(Book $book, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        if ($book->isAvailable()) {
+            $book->setIsAvailable(false);
+            $entityManager->persist($book);
+            $entityManager->flush();
+            $this->addFlash('success', 'Le livre a été emprunté avec succès !');
+        } else {
+            $this->addFlash('error', 'Ce livre n\'est pas disponible.');
+        }
+
+        return $this->redirectToRoute('book_show', ['id' => $book->getId()]);
     }
 }
