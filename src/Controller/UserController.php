21648cc9f3ebc1638a4e8d35a856c524118c2efa
dimitri78700 <dimitriraievsky@icloud.com>
2025-03-13 11:51:08 +0100

    Mettre à jour les annotations de routage dans les contrôleurs

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,7 +6,7 @@ use App\Entity\User;
 use App\Repository\LoanRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
