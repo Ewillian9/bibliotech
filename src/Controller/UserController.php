46d2f8c6f1e80d339643254593e6b8493f09ebe2
dimitri78700 <dimitriraievsky@icloud.com>
2025-03-13 11:18:29 +0100

    Mettre à jour le contrôleur User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,32 +5,16 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\LoanRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * Retourne le profil d'un utilisateur en JSON.
-     * URL : GET /{id}/profile
-     */
-    #[Route('/{id}/profile', name: 'user_profile_json', methods: ['GET'])]
-    public function profile(User $user): JsonResponse
-    {
-        return $this->json([
-            'id'    => $user->getId(),
-            'name'  => $user->getName(),
-            'email' => $user->getEmail(),
-            'role'  => $user->getRole(),
-        ]);
-    }
-
-    /**
      * Affiche le profil de l'utilisateur via Twig.
-     * URL : GET /{id}/profile/view
+     * URL : GET /user/{id}/profile
      */
-    #[Route('/{id}/profile/view', name: 'user_profile_view', methods: ['GET'])]
+    #[Route('/user/{id}/profile', name: 'user_profile_view', methods: ['GET'])]
     public function profileView(User $user): Response
     {
         return $this->render('user/profile.html.twig', [
@@ -39,21 +23,10 @@ class UserController extends AbstractController
     }
 
     /**
-     * Retourne l'historique des emprunts en JSON.
-     * URL : GET /{id}/loans
+     * Affiche l'historique des emprunts de l'utilisateur via Twig.
+     * URL : GET /user/{id}/loans
      */
-    #[Route('/{id}/loans', name: 'loan_history_json', methods: ['GET'])]
-    public function loanHistory(User $user, LoanRepository $loanRepository): JsonResponse
-    {
-        $loans = $loanRepository->findBy(['client' => $user]);
-        return $this->json($loans);
-    }
-
-    /**
-     * Affiche l'historique des emprunts via Twig.
-     * URL : GET /{id}/loans/view
-     */
-    #[Route('/{id}/loans/view', name: 'user_loans_view', methods: ['GET'])]
+    #[Route('/user/{id}/loans', name: 'user_loans_view', methods: ['GET'])]
     public function loanHistoryView(User $user, LoanRepository $loanRepository): Response
     {
         $loans = $loanRepository->findBy(['client' => $user]);
@@ -63,4 +36,3 @@ class UserController extends AbstractController
         ]);
     }
 }
-
